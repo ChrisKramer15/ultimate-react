d029a07c00bc7fc5b9493aea9568be5198a0e3bb
Christopher <syntella.development@gmail.com>
2025-03-20 17:23:19 -0500

    listgroups practice for react

---
+++ react-app/src/components/ListGroup.tsx
@@ -0,0 +1,15 @@
+function ListGroup() {
+  const items = ["Kansas City", "Denver", "San Diego", "Oakland", "Seattle"];
+  return (
+    <>
+      <h1>List</h1>
+      <ul className="list-group">
+        {items.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default ListGroup;
